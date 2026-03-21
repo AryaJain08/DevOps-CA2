88234cb78404660d197e859af162ddc50f7001f9
AryaJain08 <arya.jain.btech2023@sitnagpur.siu.edu.in>
2026-03-21 16:58:57 +0530

    Add files via upload

---
+++ DevOps-CA2/selenium/src/FormTest.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FormTest {
+
+    public static void main(String[] args) {
+
+        System.setProperty(
+            "webdriver.chrome.driver",
+            "C:\\chromedriver\\chromedriver.exe"
+            );
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(
+            "file:///C:/ProgramData/Jenkins/.jenkins/workspace/DevOps-CA2/form/index.html"
+            );
+
+        driver.findElement(By.id("name")).sendKeys("Arya");
+
+        driver.findElement(By.id("email"))
+        .sendKeys("test@gmail.com");
+
+        driver.findElement(By.id("mobile"))
+        .sendKeys("9876543210");
+
+        driver.findElement(By.id("dept"))
+        .sendKeys("Computer");
+
+        driver.findElement(By.xpath("//input[@value='Female']"))
+        .click();
+
+        driver.findElement(By.id("comments"))
+        .sendKeys("This is very good feedback form created for testing purpose only");
+
+        driver.findElement(By.xpath("//input[@value='Submit']"))
+        .click();
+
+        driver.quit();
+    }
+}
